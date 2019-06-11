5a472d4e0f45adb6f61a73f1bbe9004309880bae
Samantha Kane <s.kane133@gmail.com>
2019-06-11 22:41:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,9 @@ end
 
 def remove_element_from_end_of_array(array)
    array = []
-  array.pop
+  last = array.pop 
+  puts array.inspect
+  puts last
 end
 
 def remove_element_from_start_of_array(array)
