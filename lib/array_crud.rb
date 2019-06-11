f7a6be488c6df0c691c21c8b69c53695810a9e14
Samantha Kane <s.kane133@gmail.com>
2019-06-11 22:36:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = [1,2,3,4]
+  array.pop
 end
 
 def retrieve_element_from_index(array, index_number)
