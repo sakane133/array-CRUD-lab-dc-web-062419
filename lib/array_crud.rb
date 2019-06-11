d1e882368b7c903b663a0ce475d1bb9833170d62
Samantha Kane <s.kane133@gmail.com>
2019-06-11 22:47:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,12 +17,10 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-   array = [1,2,3]
     array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = []
   array.shift
 end
 
